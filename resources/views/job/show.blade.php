670f6ca3333f4397147e60c3aefb77acc55c6183
RaminTuzelis <k.mickunas@gmail.com>
2024-04-24 21:58:23 +0300

    Employer - Other Employer Jobs on the Job Page

--- resources/views/job/show.blade.php
+++ resources/views/job/show.blade.php
@@ -6,4 +6,28 @@
             {!! nl2br(e($job->description)) !!}}
         </p>
      </x-job-card>
+
+    <x-card class="mb-4">
+        <h2 class="mb-4 text-lg font-medium">
+            More {{ $job->employer->company_name }} Jobs
+        </h2>
+
+        <div class="text-sm text-slate-500">
+            @foreach($job->employer->jobs as $otherjob)
+                <div class="mb-4 flex justify-between">
+                    <div>
+                        <div class="text-slate-700">
+                            <a href="{{route('jobs.show', $otherjob)}}">{{$otherjob->title}}</a>
+                        </div>
+                        <div class="text-xs">
+                            {{$otherjob->created_at->diffForHumans()}}
+                        </div>
+                    </div>
+                    <div class="text-xs">
+                        € {{ $otherjob->salary }}
+                    </div>
+                </div>
+            @endforeach
+        </div>
+    </x-card>
 </x-layout>
