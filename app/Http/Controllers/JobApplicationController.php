455026da0d41be82556af1a1852ba17814798fc0
RaminTuzelis <k.mickunas@gmail.com>
2024-04-28 17:41:57 +0300

    Applying for Jobs: The Job Policy

--- app/Http/Controllers/JobApplicationController.php
+++ app/Http/Controllers/JobApplicationController.php
@@ -10,6 +10,8 @@ class JobApplicationController extends Controller
 
     public function create(Job $job)
     {
+
+        $this->authorize('apply', $job);
         return view('job_application.create', ['job' => $job]);
     }
 
