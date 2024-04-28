8aa1875e875dd203cc5486ee5e8fa09704780f16
RaminTuzelis <k.mickunas@gmail.com>
2024-04-28 14:35:38 +0300

    Applying for Jobs: Controller, Routing and Application Form

---
+++ app/Http/Controllers/JobApplicationController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Job;
+use Illuminate\Http\Request;
+
+class JobApplicationController extends Controller
+{
+
+    public function create(Job $job)
+    {
+        return view('job_application.create', ['job' => $job]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
