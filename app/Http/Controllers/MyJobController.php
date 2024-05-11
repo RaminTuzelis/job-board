edd88dc61a2d03c3adc698b348306022b8a2db70
RaminTuzelis <k.mickunas@gmail.com>
2024-05-11 18:14:29 +0300

    Employer: Job Policy

--- app/Http/Controllers/MyJobController.php
+++ app/Http/Controllers/MyJobController.php
@@ -13,6 +13,8 @@ class MyJobController extends Controller
      */
     public function index()
     {
+        $this->authorize('viewAnyEmployer', Job::class);
+
         return view('my_job.index', [
             'jobs' => auth()->user()->employer
             ->jobs()
@@ -26,6 +28,7 @@ class MyJobController extends Controller
      */
     public function create()
     {
+        $this->authorize('create', Job::class);
         return view('my_job.create');
     }
 
@@ -34,7 +37,7 @@ class MyJobController extends Controller
      */
     public function store(JobRequest $request)
     {
-
+        $this->authorize('create', Job::class);
         auth()->user()->employer->jobs()->create($request->validated());
 
         return redirect()->route('my-jobs.index')->with('success', 'Job created successfully!');
@@ -45,6 +48,7 @@ class MyJobController extends Controller
      */
     public function edit(Job $myJob)
     {
+        $this->authorize('update', $myJob);
         return view('my_job.edit', ['job' => $myJob]);
     }
 
@@ -53,6 +57,7 @@ class MyJobController extends Controller
      */
     public function update(JobRequest $request, Job $myJob)
     {
+        $this->authorize('update', $myJob);
         $myJob->update($request->validated());
 
         return redirect()->route('my-jobs.index')
